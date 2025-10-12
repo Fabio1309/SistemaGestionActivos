a38eacd93c9cdf560caa57dda292613fca30a264
jareth182 <jarethtrujilloticse@gmail.com>
2025-10-12 15:36:19 -0500

    creación de modelos y migrations

---
+++ AssetManager/Data/Migrations/20251012200333_AddIdentitySchema.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssetManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIdentitySchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaNacimiento",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "NombreCompleto",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FechaNacimiento",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "NombreCompleto",
+                table: "AspNetUsers");
+        }
+    }
+}
