65c2e46839deb3070cb8ee216ace8613e756efa4
51996 <saavedragarciafabio@gmail.com>
2025-10-20 23:40:12 -0500

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SistemaGestionActivos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
