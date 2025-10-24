8a5aa67954a2f37442aae7590222b4d721950532
jareth182 <jarethtrujilloticse@gmail.com>
2025-10-24 12:25:24 -0500

    Implementacion de vistas login y creacion de model usuario

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,15 +1,26 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SistemaGestionActivos.Models;
 
 namespace SistemaGestionActivos.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : IdentityDbContext<Usuario> 
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
     }
-    public DbSet<SistemaGestionActivos.Models.Categoria> Categorias { get; set; }
-    public DbSet<SistemaGestionActivos.Models.Ubicacion> Ubicaciones { get; set; }
-    public DbSet<SistemaGestionActivos.Models.Activo> Activos { get; set; }
+
+    public DbSet<Activo> Activos { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Ubicacion> Ubicaciones { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(options =>
+        {
+            options.MigrationsAssembly("AssetManager"); 
+        });
+        base.OnConfiguring(optionsBuilder);
+    }
 }
