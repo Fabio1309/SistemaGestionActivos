2c81100a2c8f7d9441a5750b97d644f097eafb97
jareth182 <jarethtrujilloticse@gmail.com>
2025-10-24 13:38:09 -0500

    Arreglo de reconocimiento de la clase usuario y renderizado de vistas

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -14,13 +14,4 @@ public class ApplicationDbContext : IdentityDbContext<Usuario>
     public DbSet<Activo> Activos { get; set; }
     public DbSet<Categoria> Categorias { get; set; }
     public DbSet<Ubicacion> Ubicaciones { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlite(options =>
-        {
-            options.MigrationsAssembly("AssetManager"); 
-        });
-        base.OnConfiguring(optionsBuilder);
-    }
 }
